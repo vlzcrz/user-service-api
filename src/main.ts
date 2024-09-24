6e0ae2501226618bbc6540eeeea3a7b9612819d4
vlzcrz <velizcruzv@gmail.com>
2024-09-24 17:39:54 -0300

    Agrego la variable de ambiente APP PORT

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const portListen = process.env.APP_PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -9,6 +10,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }))
-  await app.listen(3001);
+  await app.listen(portListen);
 }
 bootstrap();
